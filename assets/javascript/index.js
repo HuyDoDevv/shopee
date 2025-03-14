293423c2d2df11a390215e5413515e72bc2716d9
huydodev <doquanghuy2711@gmail.com>
2025-03-14 22:40:49 +0700

    add flash sale demo

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -39,20 +39,26 @@ function showSlides(n) {
 showSlides(slideIndex);
 startSlideshow();
 
-
+let currentPage = 1;
 function toggleHomeCategory(direction) {
+  const homeCategoryWrap = document.querySelector('.home-category__content-wrap');
   const homeCategoryList = document.querySelector('.home-category__content-list');
+  const homeCategoryListItem = document.querySelectorAll('.home-category__content--item');
   const btnNext = document.querySelector('.home-category__content-btn-right');
   const btnBack = document.querySelector('.home-category__content-btn-left');
 
-  if (direction === 'next') {
-    homeCategoryList.style.transform = 'translateX(-32%)';
-    btnNext.style.display = 'none';
-    btnBack.style.display = 'block';
-  } else {
-    homeCategoryList.style.transform = 'translateX(0)';
-    btnNext.style.display = 'block';
-    btnBack.style.display = 'none';
-  }
-}
+  const parentWidth = homeCategoryWrap.clientWidth;
 
+  const totalItems = homeCategoryListItem.length;
+  const itemsPerPage = Math.floor(parentWidth / homeCategoryListItem[0].clientWidth) * 2;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  if (direction === 'next' && currentPage < totalPages) currentPage++;
+  if (direction === 'back' && currentPage > 1) currentPage--;
+
+  const newTranslateX = -(currentPage - 1) * parentWidth;
+  homeCategoryList.style.transform = `translateX(${newTranslateX}px)`;
+
+  btnBack.style.display = currentPage === 1 ? 'none' : 'block';
+  btnNext.style.display = currentPage === totalPages ? 'none' : 'block';
+}
